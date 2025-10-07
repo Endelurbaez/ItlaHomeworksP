c2de4e7b2ec2c782d8ce23a706aa27663da391d0
Endel <Endelrafael@outlook.es>
2025-10-07 19:47:27 -0400

    Adding Homework 1

---
+++ Homework1/SistemaComunidad2/Form1.cs
@@ -0,0 +1,15 @@
+namespace SistemaComunidad2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+
+        }
+    }
+}

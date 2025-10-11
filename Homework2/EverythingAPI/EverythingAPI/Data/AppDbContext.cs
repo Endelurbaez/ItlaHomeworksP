2bc4462dea5253cc7f781d06a69221e1214cc7be
Endel <Endelrafael@outlook.es>
2025-10-11 19:46:13 -0400

    Adding Homework2

---
+++ Homework2/EverythingAPI/EverythingAPI/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EverythingAPI.Models;
+
+
+namespace EverythingAPI.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
